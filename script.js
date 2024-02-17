5648771ff846336308796dba0bef68c26f6ed8ae
Mogleson Maciel <moglesonlima@gmail.com>
2024-02-17 19:38:09 -0300

    feat: Atualização do link das imagens.

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const BASE_URL = "https://thatcopy.pw/catapi/rest/"
+const BASE_URL = "https://api.thecatapi.com/v1/images/search"
 
 const catchBtn = document.getElementById('change-cat');
 const catImg = document.getElementById('cat');
 
 const getCats = async () => {
+
     try{
         const data = await fetch(BASE_URL);
     const json = await data.json();
-
-    return json.webpurl;
+    let url = json[0].url;
+    return url;
     } catch (e){
         console.log(e.message);
     }    
